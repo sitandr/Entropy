cb3c1937beeb93f57d3767ce23c2c9c94eb5c395
sitandr <andr-sitnikov@mail.ru>
2023-02-04 19:11:43 +0300

    Basic port to net7 (lots of work still stays)

--- Rottytooth.Entropy.Compiler/CodeGenerator.cs
+++ Rottytooth.Entropy.Compiler/CodeGenerator.cs
@@ -4,6 +4,9 @@ using System.CodeDom.Compiler;
 using System.Linq;
 using System.Text;
 using Microsoft.CSharp;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System.IO;
 
 namespace Rottytooth.Entropy
 {
@@ -11,7 +14,7 @@ namespace Rottytooth.Entropy
     {
         internal static void Compile(string cSharpCode, string outputFileName)
         {
-            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            /*CSharpCodeProvider codeProvider = new Microsoft.CodeDom.Providers.DotNetCompilerPlatform.CSharpCodeProvider();
 
             System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
             parameters.ReferencedAssemblies.Add("Rottytooth.Entropy.dll");
@@ -27,6 +30,50 @@ namespace Rottytooth.Entropy
                         "Line number " + CompErr.Line + 
                          ", Error Number: " + CompErr.ErrorNumber + 
                          ", '" + CompErr.ErrorText + ";\n");
+                }
+            }*/
+
+            var syntaxTree = CSharpSyntaxTree.ParseText(cSharpCode);
+            Console.WriteLine(typeof(object).Assembly.Location);
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                "output.exe",
+                new[] { syntaxTree },
+                new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                        MetadataReference.CreateFromFile("Rottytooth.Entropy.dll"),
+                        MetadataReference.CreateFromFile(typeof(object).Assembly.Location + "/../netstandard.dll"),
+                        MetadataReference.CreateFromFile(typeof(object).Assembly.Location + "/../System.Runtime.dll"),
+                        MetadataReference.CreateFromFile(typeof(object).Assembly.Location + "/../System.Console.dll")
+                },
+
+                new CSharpCompilationOptions(OutputKind.ConsoleApplication, optimizationLevel: OptimizationLevel.Release));
+            
+            using (var stream = new MemoryStream())
+            {
+                    var emitResult = compilation.Emit(stream);
+                    if (!emitResult.Success)
+                    {
+                        foreach (var err in emitResult.Diagnostics)
+                        {
+                            Console.Error.WriteLine(err);
+                        }
+                    }
+                else
+                {
+                    using (FileStream file = File.Create(outputFileName))
+                    {
+                        stream.Seek(0, SeekOrigin.Begin);
+                        stream.CopyTo(file);
+                    }
+                    File.WriteAllText("One.runtimeconfig.json", @$"{{
+                        ""runtimeOptions"": {{
+                            ""tfm"": ""net{Environment.Version.Major}.{Environment.Version.Minor}"",
+                            ""framework"": {{
+                                ""name"": ""Microsoft.NETCore.App"",
+                                ""version"": ""{Environment.Version.Major}.{Environment.Version.Minor}.{Environment.Version.Build}""
+                            }}
+                        }}
+                    }}");
                 }
             }
 
